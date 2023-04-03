48ad50c059f24c01890c2f8188afb49e1f3ba949
SylvainPhilipona <sylvain.philipona.sp@gmail.com>
2023-04-03 11:06:45 +0200

    Resolved project breaking

--- Space-Invaders/Space-Invaders/Program.cs
+++ Space-Invaders/Space-Invaders/Program.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace Space_Invaders
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
             //Console encoding
             Console.OutputEncoding = Encoding.Unicode;
